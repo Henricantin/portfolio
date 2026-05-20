39dc42d8d07d01de9821eda569e624d1df18c294
Henricantin <henrique.barrosleo@hotmail.com>
2026-05-20 19:18:07 -0300

    update description

--- src/sections/about/index.tsx
+++ src/sections/about/index.tsx
@@ -31,23 +31,21 @@ function About() {
 						</div>
 
 						<p className={styles.description}>
-							Minha carreira em desenvolvimento começou em 2023, quando decidi
+							Minha carreira em tecnologia começou em 2023, quando decidi
 							retornar ao Brasil após uma experiência de trabalho na Irlanda.
 							Desde então, me dedico a evoluir minhas habilidades em
-							desenvolvimento web, com foco em Front-end.
+							desenvolvimento, com foco em Front-end.
 							<br />
 							<br />
-							Minha afeição por tecnologia e design me levou a explorar diversas
-							áreas, desde a criação de interfaces intuitivas até a
-							implementação de funcionalidades complexas.
+							Hoje, atuo principalmente com React, TypeScript e Tailwind CSS,
+							sendo responsável por criar páginas responsivas, interativas e
+							agradáveis aos olhos do usuário.
 							<br />
 							<br />
-							Hoje, atuo principalmente com React, TypeScript, Tailwind CSS,
-							sendo responsável por criar páginas web responsivas, interativas e
-							agradáveis aos olhos dos usuários. Creio que minha escolha pela
-							parte visual do mundo Dev se deve à minha antiga paixão no mundo
-							da comunicação, quando tive a oportunidade de me tornar bacharel
-							em Jornalismo, no ano de 2022.
+							Apesar da minha concentração em Front-end, tive a oportunidade de
+							construir projetos em Python e PHP, incluindo com banco de dados,
+							MySQL. E também, integração de APIs, no âmbito profissional,
+							utilizando o Bruno API e Swagger.
 						</p>
 					</div>
 				</div>
