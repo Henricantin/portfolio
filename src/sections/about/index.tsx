5326a48f398db0718922e0fa6f81c5f9b29a6eeb
Henricantin <henrique.barrosleo@hotmail.com>
2026-05-19 21:27:01 -0300

    update index about again

--- src/sections/about/index.tsx
+++ src/sections/about/index.tsx
@@ -4,21 +4,49 @@ function About() {
 	return (
 		<section id="about" className={styles.about}>
 			<div className={`container ${styles.content}`}>
-				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+				<div className={styles.header}>
+					<span>Conheça mais</span>
 
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quas
-					omnis harum voluptate, facilis architecto neque eum explicabo quo
-					sequi, porro, sint voluptatem assumenda! Velit ipsa animi inventore
-					architecto sit.
-				</p>
+					<h2>Sobre mim</h2>
+				</div>
 
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-					adipisci tempore quisquam dolor laboriosam itaque neque eum, molestias
-					recusandae molestiae obcaecati dignissimos sed corrupti eaque, esse
-					expedita. Corrupti, dolore eveniet.
-				</p>
+				<div className={styles.aboutGrid}>
+					<div className={styles.imageWrapper}>
+						<img src="/profile.jpg" alt="Henrique Leonardo" />
+					</div>
+
+					<div className={styles.info}>
+						<div className={styles.cards}>
+							<article className={styles.card}>
+								<span>Graduações</span>
+
+								<strong>2+ anos</strong>
+
+								<p>Desenvolvimento Front-end</p>
+							</article>
+
+							<article className={styles.card}>
+								<span>Stacks</span>
+
+								<strong>React & TypeScript</strong>
+
+								<p>Tailwind CSS</p>
+							</article>
+						</div>
+
+						<p className={styles.description}>
+							Desenvolvedor Front-end focado na criação de interfaces modernas,
+							responsivas e acessíveis. Atualmente atuo no desenvolvimento de
+							sistemas internos e aplicações web utilizando React, Angular e
+							TypeScript.
+							<br />
+							<br />
+							Minha experiência internacional na Irlanda ampliou minha
+							comunicação, adaptabilidade e visão profissional, características
+							que hoje aplico no desenvolvimento de produtos digitais.
+						</p>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
